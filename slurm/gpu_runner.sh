eaf2b7ea0bea2956bba7ea28069ec2237f273389
bbradt <bbaker43@gsu.edu>
2022-12-14 02:37:50 +0000

    added more generic slurm script

---
+++ slurm/gpu_runner.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p qTRDGPUL,qTRDGPUM,qTRDGPUH
+#SBATCH --mem=20gb
+#SBATCH --gres=gpu:1
+#SBATCH -t 24:00:00
+#SBATCH -A trends53c17
+#SBATCH --oversubscribe
+echo ARGS "${@:1}"
+eval "$(conda shell.bash hook)"
+conda activate catalyst3.9
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/users2/bbaker/miniconda3/lib python main.py "${@:1}"
+
